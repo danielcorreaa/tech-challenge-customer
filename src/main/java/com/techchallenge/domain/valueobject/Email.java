e186233f80d9d99b172747f0111cac0c30781668
danielcorreaa <daniel.cor@outlook.com>
2024-01-17 18:28:36 -0300

    correção de regex sonar

--- src/main/java/com/techchallenge/domain/valueobject/Email.java
+++ src/main/java/com/techchallenge/domain/valueobject/Email.java
@@ -1,18 +1,15 @@
 package com.techchallenge.domain.valueobject;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 
 public class Email {
 
 	private String value;
-	private static final String REGEX = "^(.+)@(.+)$";
 	public Email(String value) {
 		this.value = validate(value);
 	}
 
 	private String validate(String value) {
-	    if(Pattern.compile(REGEX).matcher(value).matches()) {
+		if(value.contains("@")){
 	    	return value;
 	    }
 	    throw new IllegalArgumentException("Invalid Email!");
