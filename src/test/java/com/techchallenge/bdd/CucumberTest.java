186987ef250e387507953d9ed24f18149090a0b3
danielcorreaa <daniel.cor@outlook.com>
2024-01-26 00:49:40 -0300

    test

--- src/test/java/com/techchallenge/bdd/CucumberTest.java
+++ src/test/java/com/techchallenge/bdd/CucumberTest.java
@@ -4,6 +4,7 @@ package com.techchallenge.bdd;
 import org.junit.platform.suite.api.IncludeEngines;
 import org.junit.platform.suite.api.SelectClasspathResource;
 import org.junit.platform.suite.api.Suite;
+import org.springframework.test.context.ActiveProfiles;
 
 @Suite
 @IncludeEngines("cucumber")
