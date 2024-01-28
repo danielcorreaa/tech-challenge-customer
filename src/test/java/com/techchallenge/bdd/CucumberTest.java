a0d7785ba8da2474904b9f308c5b0a572d0791c2
danielcorreaa <daniel.cor@outlook.com>
2024-01-28 11:44:52 -0300

    ajustes nos testes

--- src/test/java/com/techchallenge/bdd/CucumberTest.java
+++ src/test/java/com/techchallenge/bdd/CucumberTest.java
@@ -4,7 +4,6 @@ package com.techchallenge.bdd;
 import org.junit.platform.suite.api.IncludeEngines;
 import org.junit.platform.suite.api.SelectClasspathResource;
 import org.junit.platform.suite.api.Suite;
-import org.springframework.test.context.ActiveProfiles;
 
 @Suite
 @IncludeEngines("cucumber")
